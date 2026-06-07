c5eb63b56896307b00e9af6dd3688d8618c091f3
leoncaballer <leoncaballer@gmail.com>
2026-06-07 10:23:29 +0200

    allowedOrigins update

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 const allowedOrigins = [
+  "https://f003.backblazeb2.com",
+  "https://game.mtvreginald.com",
   "https://html-classic.itch.zone",
   "http://localhost:3000",
   "http://localhost:5173"
